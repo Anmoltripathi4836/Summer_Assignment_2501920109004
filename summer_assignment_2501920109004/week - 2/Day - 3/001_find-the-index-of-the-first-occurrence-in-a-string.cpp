0edd3762ab0e4a5a86c30ca861a3b3ae2e96e8f5
Anmoltripathi4836 <anmoltripathi44886@gmail.com>
2026-06-10 18:48:00 +0530

    Uploaded week-2 day-3 assignments

---
+++ summer_assignment_2501920109004/week - 2/Day - 3/001_find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i = 0; i < haystack.length(); i++){
+            int j;
+            for(j = 0; j < needle.length(); j++){
+                if(i + j >= haystack.length() || haystack[i + j] != needle[j]){
+                    break;
+                }
+            }
+            if(j == needle.length()){
+            return i; 
+        }        
+        }
+        return -1;       
+    }
+};
